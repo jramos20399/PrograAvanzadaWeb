add35153fa812e65b22ab922abf5afd3010bed64
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-01-31 18:45:50 -0600

    CRUD Shippers Completo

---
+++ Northwind/BackEnd/Controllers/ShipperController.cs
@@ -0,0 +1,84 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipperController : ControllerBase
+    {
+        private IShipperDAL shipperDAL;
+
+        public ShipperController()
+        {
+            shipperDAL = new ShipperDALImpl(new NorthWindContext());
+        }
+
+        #region Consultar
+        // GET: api/<ShipperController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+           
+            IEnumerable<Shipper> shippers= shipperDAL.GetAll();
+
+
+            return new JsonResult(shippers);
+        }
+
+        // GET api/<ShipperController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+
+            Shipper shipper = shipperDAL.Get(id);
+            return new JsonResult(shipper);
+        }
+
+        #endregion
+
+        #region Agregar
+
+        // POST api/<ShipperController>
+        [HttpPost]
+        public JsonResult Post([FromBody] Shipper shipper)
+        {
+            shipperDAL.Add(shipper);
+            return new JsonResult(shipper);
+
+        }
+
+        #endregion
+        
+        #region Actualizar
+        [HttpPut]
+        public JsonResult Put([FromBody] Shipper shipper)
+        {
+
+            shipperDAL.Add(shipper);
+            return new JsonResult(shipper);
+
+        }
+
+        #endregion
+
+
+        #region Eliminar
+        // DELETE api/<ShipperController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Shipper shipper = new Shipper { ShipperId= id };
+            shipperDAL.Remove(shipper);
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ Northwind/DAL/Implementations/ShipperDALImpl.cs
@@ -0,0 +1,123 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class ShipperDALImpl : IShipperDAL
+    {
+        private NorthWindContext context;
+
+        public ShipperDALImpl(NorthWindContext _context)
+        {
+            context= _context;
+        }
+
+
+        public bool Add(Shipper entity)
+        {
+            try
+                {
+
+                using (UnidadDeTrabajo<Shipper> unidad = new UnidadDeTrabajo<Shipper>(context))
+                {
+                    unidad.genericDAL.Add(entity);
+                    unidad.Complete();
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+
+        public void AddRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Shipper> Find(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipper Get(int id)
+        {
+            Shipper shipper;
+            using (UnidadDeTrabajo<Shipper> unidad = new UnidadDeTrabajo<Shipper>(context))
+            {
+                shipper = unidad.genericDAL.Get(id);
+            }
+            return shipper;
+        }
+
+        public IEnumerable<Shipper> GetAll()
+        {
+            IEnumerable<Shipper> shippers;
+            using (UnidadDeTrabajo<Shipper> unidad = new UnidadDeTrabajo<Shipper>(context))
+            {
+                shippers = unidad.genericDAL.GetAll();
+            }
+            return shippers;
+        }
+
+        public bool Remove(Shipper entity)
+        {
+            try
+            {
+
+                using (UnidadDeTrabajo<Shipper> unidad = new UnidadDeTrabajo<Shipper>(context))
+                {
+                    unidad.genericDAL.Remove(entity);
+                    unidad.Complete();
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void RemoveRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipper SingleOrDefault(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Shipper entity)
+        {
+            try
+            {
+
+                using (UnidadDeTrabajo<Shipper> unidad = new UnidadDeTrabajo<Shipper>(context))
+                {
+                    unidad.genericDAL.Update(entity);
+                    unidad.Complete();
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}

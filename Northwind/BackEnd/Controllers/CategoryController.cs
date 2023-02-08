a467f5d95396b5910a44babdc4d7551d5c8f0b8a
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-02-07 18:34:55 -0600

    Cambio de Entity a Model

--- Northwind/BackEnd/Controllers/CategoryController.cs
+++ Northwind/BackEnd/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Implementations;
+﻿using BackEnd.Models;
+using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -13,8 +14,34 @@ namespace BackEnd.Controllers
     {
 
         private ICategoryDAL categoryDAL;
-        
-        
+
+        #region Convertir
+        private CategoryModel Convertir(Category entity)
+        {
+            return new CategoryModel
+            {
+                CategoryId = entity.CategoryId,
+                CategoryName = entity.CategoryName,
+                Description = entity.Description
+
+
+            };
+        }
+
+
+        private Category Convertir(CategoryModel model)
+        {
+            return new Category
+            {
+                CategoryId = model.CategoryId,
+                CategoryName = model.CategoryName,
+                Description = model.Description
+
+
+            };
+        }
+        #endregion
+
         #region Constructor
         public CategoryController()
         {
@@ -30,8 +57,14 @@ namespace BackEnd.Controllers
         {
             IEnumerable<Category> categories = categoryDAL.GetAll();
 
+            List<CategoryModel> lista = new List<CategoryModel>();
 
-            return new JsonResult(categories);
+            foreach (var category in categories) {
+                lista.Add(Convertir(category)                       
+                    
+                    );
+            }
+            return new JsonResult(lista);
         }
 
         // GET api/<CategoryController>/5
@@ -41,8 +74,9 @@ namespace BackEnd.Controllers
             Category category;
             category = categoryDAL.Get(id);
 
+           
 
-            return new JsonResult(category);
+            return new JsonResult(Convertir(category));
 
         }
         #endregion
@@ -51,10 +85,10 @@ namespace BackEnd.Controllers
         #region Agregar
         // POST api/<CategoryController>
         [HttpPost]
-        public JsonResult Post([FromBody] Category category)
+        public JsonResult Post([FromBody] CategoryModel category)
         {
-            categoryDAL.Add(category);
-            return new JsonResult(category);
+            categoryDAL.Add(Convertir(category));
+            return new JsonResult(Convertir(category));
 
         }
 
@@ -64,11 +98,11 @@ namespace BackEnd.Controllers
         #region MOdificar
         // PUT api/<CategoryController>/5
         [HttpPut]
-        public JsonResult Put([FromBody] Category category)
+        public JsonResult Put([FromBody] CategoryModel category)
         {
 
-            categoryDAL.Update(category);
-            return new JsonResult(category);    
+            categoryDAL.Update(Convertir(category));
+            return new JsonResult(Convertir(category));
 
         }
         #endregion
@@ -82,7 +116,7 @@ namespace BackEnd.Controllers
             Category category = new Category { CategoryId= id };
             categoryDAL.Remove(category);
 
-            return new  JsonResult(category);
+            return new  JsonResult(Convertir(category));
 
 
         }
---
+++ Northwind/BackEnd/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models
+{
+    public class CategoryModel
+    {
+
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; } = null!;
+        public string? Description { get; set; }
+        
+    }
+}

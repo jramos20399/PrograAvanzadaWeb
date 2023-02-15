70696caef198e28c335ff4c3c5f78ef3d0eea0d8
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-02-14 19:10:31 -0600

    Lista De Productos Terminada

---
+++ Northwind/FrontEnd/Helpers/ProductHelper.cs
@@ -0,0 +1,33 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class ProductHelper
+    {
+        private ServiceRepository ServiceRepository;
+
+
+        public ProductHelper() {
+            ServiceRepository= new ServiceRepository();
+        
+        }  
+
+
+
+        public List<ProductViewModel> GetAll()
+        {
+            List<ProductViewModel> lista = new List<ProductViewModel>();
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/product");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ProductViewModel>>(content);
+            }
+
+            return lista;
+
+        }
+    }
+}
